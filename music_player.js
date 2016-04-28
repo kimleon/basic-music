3ab7e6dbec5a107abd4ef8629b4692dd8b68da3a
kimleon <kimleon@mit.edu>
2016-04-28 09:49:11 -0400

    ytplayer back in html

--- music_player.js
+++ music_player.js
@@ -14,50 +14,6 @@ $(function() {
 	var current_mp3;
   var url_songs = {};
 	var all_songs = [];
-  var player;
-
-
-  // 2. This code loads the IFrame Player API code asynchronously.
-  var tag = document.createElement('script');
-
-  tag.src = "https://www.youtube.com/iframe_api";
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-  // 3. This function creates an <iframe> (and YouTube player)
-  //    after the API code downloads.
-  function onYouTubeIframeAPIReady() {
-    player = new YT.Player('YTplayer', {
-      height: '273',
-      width: '448',
-      // display: 'none',
-      videoId: 'i4ooH8frBWg',
-      events: {
-        'onReady': onPlayerReady,
-        'onStateChange': onPlayerStateChange
-      }
-    });
-  }
-
-  // 4. The API will call this function when the video player is ready.
-  function onPlayerReady(event) {
-    // event.target.playVideo();
-    console.log("player ready")
-  }
-
-  // 5. The API calls this function when the player's state changes.
-  //    The function indicates that when playing a video (state=1),
-  //    the player should play for six seconds and then stop.
-  var done = false;
-  function onPlayerStateChange(event) {
-    if(event.data === 0) {          
-      console.log('video done');
-      // go onto the next song in the playlist
-    }
-  }
-  function stopVideo() {
-    player.stopVideo();
-  }
 
 
 	$('#itunes-upload').change(function(){
